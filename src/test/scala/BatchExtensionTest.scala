d71b77c8610793113c63c8b053399bd07cce4ed3
Max De Marzi <maxdemarzi@hotmail.com>
2016-02-05 17:17:08 -0600

    with batch extension

---
+++ src/test/scala/BatchExtensionTest.scala
@@ -0,0 +1,53 @@
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+class BatchExtensionTest extends Simulation {
+  val httpConf = http
+    .baseURL("http://localhost:7474")
+    .acceptHeader("application/json")
+    .shareConnections
+  //  Uncomment to see the response of each request.
+  //  .extraInfoExtractor(extraInfo => {
+  //    println(extraInfo.response.body.string)
+  //    Nil
+  //  }).disableResponseChunksDiscarding
+
+
+  val random = new util.Random
+  val feeder = Iterator.continually(Map("user_id" -> random.nextInt()))
+
+  val cypher = """CREATE ( me:User { user_id: {user_id} } )"""
+  val statements = """{"statements" : [{"statement" : "%s", "parameters": {"user_id": %s}}]}"""
+
+  val setup = scenario("Create Index")
+    .exec(http("Create Index")
+      .post("/db/data/schema/index/User")
+      .basicAuth("neo4j", "swordfish")
+      .body(StringBody("""{"property_keys" : ["user_id"] } """))
+      .asJSON
+      .check(status.in(200,409))
+      .silent
+    )
+
+  val scn = scenario("Create Indexed Nodes via Extension")
+    .during(30 seconds) {
+      feed(feeder)
+        .exec(
+          http("create indexed node via ext")
+            .post("/v1/service/batch")
+            .basicAuth("neo4j", "swordfish")
+            .body(StringBody(statements.format(cypher, "${user_id}")))
+            .asJSON
+            .check(status.is(200))
+        )
+    }
+
+  setUp(
+    setup.inject( atOnceUsers(1) ).protocols(httpConf),
+    scn.inject( atOnceUsers(8) ).protocols(httpConf)
+  )
+
+}
